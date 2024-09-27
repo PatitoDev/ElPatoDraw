3260396ed068e577ae639e5a32617099e7b5a655
Dani Rodríguez <dani@danirod.es>
2024-09-27 18:12:36 +0200

    Add document search box (#4)
    Adds a simple bar that allows to filter the list of cards presented in the files list when filled with something.

--- front/src/Components/DrawingList/index.tsx
+++ front/src/Components/DrawingList/index.tsx
@@ -1,5 +1,13 @@
-import { Card, SimpleGrid, Text } from '@mantine/core';
+import {
+  Card,
+  CloseButton,
+  Group,
+  SimpleGrid,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import { DrawingMetadata } from '../../types/Entity';
+import { useEffect, useState } from 'react';
 
 export interface DrawingListProps {
   files: Array<DrawingMetadata>,
@@ -16,33 +24,73 @@ const parseDate = (date: string) => (
 );
 
 export const DrawingList = ({ files, onFileClick }: DrawingListProps) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const sortedFiles = [...files];
-  sortedFiles.sort((a, b) => {
-    const tsA = dateAsTimestamp(a.created_at);
-    const tsB = dateAsTimestamp(b.created_at);
-    return tsB - tsA;
+  const [sortedFiles, setSortedFiles] = useState<DrawingMetadata[]>([]);
+
+  useEffect(() => {
+    let realFiles: DrawingMetadata[];
+    if (!searchQuery) {
+      realFiles = [...files];
+    } else {
+      const lowerSearchQuery = searchQuery.toLocaleLowerCase();
+      realFiles = files.filter(
+        (f) => f.name.toLocaleLowerCase().indexOf(lowerSearchQuery) >= 0,
+      );
+    }
+    realFiles.sort((a, b) => {
+      const tsA = dateAsTimestamp(a.created_at);
+      const tsB = dateAsTimestamp(b.created_at);
+      return tsB - tsA;
+    });
+    setSortedFiles(realFiles);
   });
 
+  const Cancel = (
+    <CloseButton
+      variant="transparent"
+      onClick={() => setSearchQuery('')}
+      disabled={searchQuery.length === 0}
+      sx={{ ':disabled': { color: 'transparent' } }}
+    />
+  );
+
   return (
-    <SimpleGrid sx={{ padding: '1rem', gridTemplateColumns: 'repeat(auto-fit, minmax(15em, 1fr))' }} spacing="0.75rem">
-      {sortedFiles.map((file) => (
-        <Card
-          sx={{
-            transition: 'transform 0.3s, box-shadow 0.3s',
-            boxShadow: 'inset 0 0 0 0px rgb(193 194 197 / 0%)',
-            ':hover' : {
-              cursor: 'pointer',
-              boxShadow: 'inset 0 0 0 3px rgb(193 194 197 / 100%)',
-              transform: 'scale(1.03)'
-            }
-          }}
-          key={file.id} 
-          onClick={() => onFileClick(file.id)}>
-          <Text weight='bold'>{file.name}</Text>
-          <Text size="sm">{parseDate(file.created_at)}</Text>
-        </Card>
-      ))}
-    </SimpleGrid>
+    <>
+      <Group style={{ justifyContent: 'center' }}>
+        <TextInput
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.currentTarget.value)}
+          placeholder="Search..."
+          rightSection={Cancel}
+        />
+      </Group>
+      <SimpleGrid
+        sx={{
+          padding: '1rem',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(15em, 1fr))',
+        }}
+        spacing="0.75rem"
+      >
+        {sortedFiles.map((file) => (
+          <Card
+            sx={{
+              transition: 'transform 0.3s, box-shadow 0.3s',
+              boxShadow: 'inset 0 0 0 0px rgb(193 194 197 / 0%)',
+              ':hover': {
+                cursor: 'pointer',
+                boxShadow: 'inset 0 0 0 3px rgb(193 194 197 / 100%)',
+                transform: 'scale(1.03)',
+              },
+            }}
+            key={file.id}
+            onClick={() => onFileClick(file.id)}
+          >
+            <Text weight="bold">{file.name}</Text>
+            <Text size="sm">{parseDate(file.created_at)}</Text>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
