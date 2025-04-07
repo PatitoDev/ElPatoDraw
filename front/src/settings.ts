297949566bf986e4cc02239fcaba57f2fbd973eb
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2025-04-07 23:06:07 +0100

    add api url

--- front/src/settings.ts
+++ front/src/settings.ts
@@ -2,3 +2,8 @@ export const SUPABASE_CONFIG = {
   url: 'https://qvvzhuuxhdxpkbeaasqp.supabase.co',
   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dnpodXV4aGR4cGtiZWFhc3FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQyNTg2MTgsImV4cCI6MTk5OTgzNDYxOH0.Wx5fbJtRDFTizlNBgn4VvgJVK6L0OZ2KhHWRGORXaZ0',
 };
+
+export const API_URLS = {
+  worker: 'https://elpatodraw-worker.niv3k-business.workers.dev',
+  metadataApi: 'https://api.elpato.dev/PatoDrawApi'
+}
